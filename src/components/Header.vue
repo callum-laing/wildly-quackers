1edcd502a614b73d2aa1d1e8132d4bda6b222ffb
Callum Laing <callum.laing91@hotmail.co.uk>
2024-12-26 14:55:02 +0000

    hero added

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>WILDLY QUACKERS</h1>
+  <div class="header-links">
+    <a href="/">HOME</a>
+    <a href="">SHOP</a>
+    <a href="/">CONTACT</a>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2.5rem;
+  text-align: center;
+  padding: 20px;
+  font-family: lora;
+  font-weight: 600;
+  letter-spacing: 5px;
+  text-decoration: underline;
+  text-decoration-color: rgb(50, 143, 75);
+  text-decoration-thickness: 2px;
+  text-underline-offset: 20px;
+  padding-bottom: 40px;
+}
+
+.header-links {
+  text-align: center;
+}
+
+.header-links > a {
+  text-decoration: none;
+  margin: 0 20px;
+  color: black;
+  letter-spacing: 2px;
+}
+
+.header-links > a:hover {
+  color: rgb(50, 143, 75);
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hero">
+    <div class="card">
+      <img src="../assets/dogs.jpg" alt="Two dogs on a bed" />
+    </div>
+  </div>
+  <div>
+    <h2>Discover the Heart of Artisan Crafting</h2>
+  </div>
+</template>
+
+<style scoped>
+.hero {
+  margin-top: 50px;
+  min-height: 50dvh;
+  background-image: url(../assets/wild.png);
+  background-size: 90%;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
+  position: relative;
+}
+
+.card {
+  background-color: white;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  max-width: 400px;
+  text-align: center;
+}
+
+.card img {
+  width: 100%;
+  border-radius: 10px;
+  margin-bottom: 10px;
+}
+
+h2 {
+  font-size: 3rem;
+  text-align: center;
+}
+</style>
