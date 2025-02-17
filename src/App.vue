7c796917ddf8a1222ca45a23f3df568f2e10bbbb
Callum Laing <callum.laing91@hotmail.co.uk>
2025-02-17 01:32:29 +0000

    tweaked navbar to no longer use ref and scrollto js code

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,33 @@
 <script setup>
-import { ref } from 'vue'
 import Header from './components/Header.vue'
 import Hero from './components/Hero.vue'
 import Discover from './components/Discover.vue'
 import Products from './components/Products.vue'
 import Contact from './components/Contact.vue'
 import Footer from './components/Footer.vue'
-
-const heroSection = ref(null)
-const productsSection = ref(null)
-const contactSection = ref(null)
-
-const scrollToSection = (section) => {
-  if (section === 'home') {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  } else if (section === 'shop') {
-    productsSection.value.scrollIntoView({ behavior: 'smooth' })
-  } else if (section === 'contact') {
-    contactSection.value.scrollIntoView({ behavior: 'smooth' })
-  }
-}
 </script>
 
 <template>
   <main>
     <Header />
-    <div class="header-links">
+    <nav class="header-links">
       <a href="#shop">SHOP</a>
       <a href="#contact">CONTACT</a>
-    </div>
+    </nav>
 
-
-      <Hero />
-
+    <Hero />
 
     <h2>Discover the Heart of Artisan Crafting</h2>
     <Discover />
 
-    <h2>Featured Products</h2>
-    <div ref="productsSection">
-      <Products />
-    </div>
+    <h2 id="shop">Featured Products</h2>
+    <Products />
 
     <h2>
       Artisan home decor inspired by nature's finest materials with designs by Delphine Brooks, Sam
       Bowker-Laing and others
     </h2>
-    <div ref="contactSection">
+    <div id="contact">
       <Contact />
     </div>
     <Footer />
