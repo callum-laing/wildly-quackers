ffab3007d3418fab72fe462d3fa12645f6f6808f
Callum Laing <callum.laing91@hotmail.co.uk>
2025-02-17 20:04:41 +0000

    added product images

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
-    <p>New products coming soon!</p>
+  <div class="shopImages">
+    <img src="../assets/shop/store.jpg" alt="store cushion" />
+    <img src="../assets/shop/set.jpg" alt="sheep cushion" />
+    <img src="../assets/shop/dogrug.jpg" alt="dog on a rug" />
+    <img src="../assets/shop/board.jpg" alt="sheep cushion" />
+    <img src="../assets/shop/sheep.jpg" alt="sheep cushion" />
+    <img src="../assets/shop/hunting.jpg" alt="sheep cushion" />
+    <img src="../assets/shop/farm.jpg" alt="sheep cushion" />
+    <img src="../assets/shop/horse.jpg" alt="sheep cushion" />
   </div>
 </template>
 
 <style scoped>
-div {
+.shopImages {
+  display: inline-grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 5em;
+  width: 100%;
+  padding: 0 10em;
+}
+
+img {
   display: flex;
-  align-items: center;
   justify-content: center;
-  min-height: 200px;
-  background-color: rgb(230, 230, 230, 0.2);
+  align-items: center;
+  box-shadow: 0 6px 8px rgba(0, 0, 0, 0.4);
+  margin: 0 auto;
+}
+
+@media (max-width: 1024px) {
+  .shopImages {
+    grid-template-columns: repeat(2, 1fr);
+    padding: 0 1em;
+  }
+}
+
+@media (max-width: 600px) {
+  .shopImages {
+    grid-template-columns: 1fr 1fr;
+    padding: 1em;
+    grid-gap: 2em;
+  }
 }
 </style>
